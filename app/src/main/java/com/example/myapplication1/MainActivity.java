9343829c5ae6fb8a4bceb8e9d6a83b9c8dfb2ab5
KiraneTMS <kirenaiasakura@gmail.com>
2019-07-18 14:02:04 +0700

    First Commit

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activityLinear);
+        setContentView(R.layout.activity_linear);
     }
 }
